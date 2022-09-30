2fb825bb2c0fc1a034d11c36653ce1099f33c098
Anthony <Darkapple22@protonmail.com>
2022-09-30 13:29:46 -0500

    Uploading Files.

---
+++ Script.js
@@ -0,0 +1,48 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled', 
+    window.scrollY > 0);
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      599: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      1023: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      }
+
+    }
+  });
+
+const nav = document.querySelector(".nav_links");
+const openNavbtn = document.querySelector("#nav_toggle-open");
+const closeNavbtn = document.querySelector("#nav_toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavbtn.style.display = "none";
+  closeNavbtn.style.display = "inline-block"
+}
+
+const closeNav = () => {
+  nav.style.display = "none";
+  closeNavbtn.style.display = "none";
+  openNavbtn.style.display = "inline-block";
+}
+
+openNavbtn.addEventListener('click', openNav);
+closeNavbtn.addEventListener('click', closeNav);
+
+nav.querySelectorAll("li a").forEach(navLink => {
+  navLink.addEventListener("click", closeNav);
+});
+
